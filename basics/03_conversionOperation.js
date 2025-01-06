cf6283bce1a408b9d0b8ccfd539411c807b38309
skshejad <48248947+skshejad@users.noreply.github.com>
2025-01-06 13:42:51 +0000

    Conversion

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = true
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = undefined
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = "12.sdfghj"
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
