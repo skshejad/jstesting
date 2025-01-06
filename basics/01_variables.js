01b8f693edd54c86d3faf37c965cbc956a31235b
skshejad <48248947+skshejad@users.noreply.github.com>
2025-01-06 13:05:10 +0000

    Datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "skshejad@gamil.com"
+var accountPassword = "12345"
+accountCity ="Bbsr" //wrong
+let accountState
+// accountId = 12
+accountEmail = "test2nd@email.com"
+accountPassword = "test1234"
+accountCity = "Bengaluru"
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
